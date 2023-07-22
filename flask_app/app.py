ecf5839169981577b1ae38c21579a53b1d27c281
Salu Teslim <saluayomide@gmail.com>
2023-07-22 20:39:02 +0100

    edit

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Yayy, we have the 2nd version of our flask application"
+    return "Yayy, we have our flask application running on AWS ECS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
